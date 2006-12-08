1594fd53d321cf24320723eaabd2ea6864146853
James Willcox <snorp@snorp.net>
2006-12-08 00:32:06 +0000

    oops again

---
+++ src/HardDiskConfigDialog.cs
@@ -0,0 +1,78 @@
+using System;
+using Mono.Unix;
+using Gtk;
+
+namespace VmxManager {
+
+    public class HardDiskConfigDialog : Dialog {
+
+        private VirtualHardDisk disk;
+
+        [Glade.Widget]
+        private Widget diskConfigContent;
+
+        [Glade.Widget]
+        private ComboBox busTypeCombo;
+
+        [Glade.Widget]
+        private ComboBox busNumberCombo;
+
+        [Glade.Widget]
+        private ComboBox deviceNumberCombo;
+
+        [Glade.Widget]
+        private SpinButton diskSizeSpin;
+
+        public HardDiskConfigDialog (VirtualHardDisk disk, bool capacitySensitive, Window parent) : base (Catalog.GetString ("Configure Hard Disk"),
+                                                                                  parent, DialogFlags.NoSeparator,
+                                                                                  Stock.Cancel, ResponseType.Cancel,
+                                                                                  Stock.Ok, ResponseType.Ok) {
+            this.disk = disk;
+
+            Glade.XML xml = new Glade.XML ("vmx-manager.glade", "diskConfigContent");
+            xml.Autoconnect (this);
+
+            VBox.Add (diskConfigContent);
+            diskConfigContent.ShowAll ();
+
+            diskSizeSpin.Sensitive = capacitySensitive;
+
+            Response += delegate (object o, ResponseArgs args) {
+                if (args.ResponseId == ResponseType.Ok) {
+                    Save ();
+                }
+                
+                this.Destroy ();
+            };
+
+            Load ();
+        }
+
+        private void Load () {
+            if (disk.BusType == DiskBusType.Ide) {
+                busTypeCombo.Active = 0;
+            } else {
+                busTypeCombo.Active = 1;
+            }
+
+            busNumberCombo.Active = disk.BusNumber;
+            deviceNumberCombo.Active = disk.DeviceNumber;
+            diskSizeSpin.Value = (double) disk.Capacity / (double) 1024 / (double) 1024 / (double) 1024;
+        }
+
+        private void Save () {
+            if (busTypeCombo.Active == 0) {
+                disk.BusType = DiskBusType.Ide;
+            } else {
+                disk.BusType = DiskBusType.Scsi;
+            }
+
+            disk.BusNumber = (ushort) busNumberCombo.Active;
+            disk.DeviceNumber = (ushort) deviceNumberCombo.Active;
+
+            if (diskSizeSpin.Sensitive) {
+                disk.Capacity = (long) (diskSizeSpin.Value * (double) 1024 * (double) 1024 * (double) 1024);
+            }
+        }
+    }
+}
